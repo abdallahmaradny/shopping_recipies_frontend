73ae59e1902d249b270b12d6c5928c90b463587f
abdallahmaradny <abdallahmaradny@gmail.com>
2020-04-24 17:47:35 +0200

    Adding recipe model data to component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
 
+
+export class RecipeListComponent implements OnInit {
+recipes:Recipe[]=[
+  new Recipe("botato","Good botato","https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"),
+  new Recipe("botato","Good botato","https://get.pxhere.com/photo/dish-food-cuisine-ingredient-produce-recipe-meat-vegetarian-food-comfort-food-side-dish-bento-Yong-tau-foo-meal-salad-teriyaki-chinese-food-japanese-cuisine-donburi-1575227.jpg")
+
+]
   constructor() { }
 
   ngOnInit(): void {
