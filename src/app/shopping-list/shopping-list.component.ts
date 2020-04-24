97198059de251d1d168d06088c815e7de606714a
abdallahmaradny <abdallahmaradny@gmail.com>
2020-04-24 17:44:09 +0200

    Adding ingredients and buttons to shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from "../shared/ingredients.model";
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+ingredients:Ingredient[]=[
+  new Ingredient("apple",10),
+  new Ingredient("onion",2)
+]
   constructor() { }
 
   ngOnInit(): void {
